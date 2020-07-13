e5cf01206afa1d59b20dfc521125a7ac48e78007
hgupta20 <harshdevprakashgupta@outlook.com>
2020-07-13 13:58:01 -0500

    Updates

---
+++ precache-manifest.fb6c227d77b0c2906ea6de345a84da5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88eb4ba66787a031995d66596de67424",
+    "url": "/deploy_test/index.html"
+  },
+  {
+    "revision": "5c9a58f35f15f4231703",
+    "url": "/deploy_test/static/css/main.58d05c08.chunk.css"
+  },
+  {
+    "revision": "fde2034d650acb5757b6",
+    "url": "/deploy_test/static/js/2.f236d9a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy_test/static/js/2.f236d9a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9a58f35f15f4231703",
+    "url": "/deploy_test/static/js/main.01ba086c.chunk.js"
+  },
+  {
+    "revision": "38cb24a7d79e70f939f2",
+    "url": "/deploy_test/static/js/runtime-main.275e3d75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
